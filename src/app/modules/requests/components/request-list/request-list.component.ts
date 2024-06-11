91a5b386f1389900095ae229676d3c833050bd63
Rodrigue Baroux <rodriguebaroux@gmail.com>
2024-06-11 11:35:34 +0200

    Add style for requests list

--- src/app/modules/requests/components/request-list/request-list.component.ts
+++ src/app/modules/requests/components/request-list/request-list.component.ts
@@ -1,13 +1,17 @@
 import { HttpClientModule } from '@angular/common/http';
 import { RequestsService } from '../../services/requests.service';
 import { MaterialsService } from '../../../materials/services/materials.service';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-request-list',
     standalone: true,
-    imports: [CommonModule, HttpClientModule],
+    imports: [CommonModule, HttpClientModule, FontAwesomeModule],
     templateUrl: './request-list.component.html',
     styleUrl: './request-list.component.css'
 })
@@ -16,16 +20,26 @@ export class RequestListComponent {
     requests: any[] = [];
     rejectionDescription: string = '';
 
-    constructor(private requestService: RequestsService) { }
+    constructor(private requestService: RequestsService, private library: FaIconLibrary, private router: Router, private titleService: Title) {
+        library.addIcons(faArrowLeft);
+        this.titleService.setTitle("Liste des demandes - Material Manageur");
+    }
 
     ngOnInit(): void {
         this.loadRequests();
     }
 
+    goBack() {
+        this.router.navigate(['/dashboard']);
+      }
+
     loadRequests(): void {
         this.requestService.getRequestsList()
             .subscribe(requests => {
-                this.requests = requests;
+                this.requests = requests.map(request => {
+                    request.status = this.transformStatus(request.status);
+                    return request;
+                });
             });
     }
 
@@ -79,5 +93,14 @@ export class RequestListComponent {
                     console.error('Erreur lors du refus du retour', error);
                 }
             });
+    }
+
+    transformStatus(status: string): string {
+        switch (status) {
+            case 'en_attente':
+                return 'En attente';
+            default:
+                return status;
+        }
     }
 }
