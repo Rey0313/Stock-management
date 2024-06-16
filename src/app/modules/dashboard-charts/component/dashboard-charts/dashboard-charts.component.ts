3c7730311caa4b19a193ae26bf372937cf8a4bc1
Lucas Ferreira <65078868+LucasFrra@users.noreply.github.com>
2024-06-16 19:14:02 +0200

    change dashboard admin

--- src/app/modules/dashboard-charts/component/dashboard-charts/dashboard-charts.component.ts
+++ src/app/modules/dashboard-charts/component/dashboard-charts/dashboard-charts.component.ts
@@ -5,11 +5,13 @@ import { RoomService } from '../../../rooms/services/room.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import swal from 'sweetalert';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dashboard-charts',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, FontAwesomeModule],
   templateUrl: './dashboard-charts.component.html',
   styleUrl: './dashboard-charts.component.css'
 })
@@ -19,9 +21,13 @@ export class DashboardChartsComponent {
   isModalOpen = false;
   roomName: string = '';
   usedMaterials: any[] = [];
+  totalMaterials: number = 0;
+  totalUsedMaterials: number = 0;
+  showAllRooms: boolean = false; 
 
-  constructor(private materialService: MaterialsService, private roomService: RoomService) {
+  constructor(private materialService: MaterialsService, private roomService: RoomService, private library: FaIconLibrary) {
     Chart.register(BarController, CategoryScale, LinearScale, BarElement);
+    this.library.addIcons(faChevronDown, faChevronUp);
   }
 
   ngOnInit() {
@@ -43,14 +49,15 @@ export class DashboardChartsComponent {
 
   getMaterialsByTypeData() {
     this.materialService.getMaterialsByType().subscribe(data => {
+      this.totalMaterials = data.values.reduce((sum: number, value: number) => sum + value, 0);
       const chartData = {
         labels: data.labels,
         datasets: [
           {
             label: 'Nombre de Matériels',
             data: data.values,
-            backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(255, 99, 132, 0.2)'],
-            borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)'],
+            backgroundColor: ['rgba(75, 192, 192, 0.5)', 'rgba(50, 49, 165, 0.5)'],
+            borderColor: ['rgba(75, 192, 192, 1)', 'rgba(50, 49, 165, 1)'],
             borderWidth: 1,
           },
         ],
@@ -115,6 +122,7 @@ export class DashboardChartsComponent {
   getUsedMaterials() {
     this.materialService.getUsedMaterials().subscribe({
       next: (usedMaterials) => {
+        this.totalUsedMaterials = usedMaterials.length;
         this.usedMaterials = usedMaterials.sort((a, b) => a.type.localeCompare(b.type));
       },
       error: (error) => {
@@ -122,5 +130,9 @@ export class DashboardChartsComponent {
       }
     });
   }
-  
+
+  toggleRoomList() {
+    this.showAllRooms = !this.showAllRooms;
+  }
+
 }
